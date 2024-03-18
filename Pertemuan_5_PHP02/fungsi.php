a04ba155b290737ad84117004b0bd96658e3022a
rizkypratamayudha <masrizki87@gmail.com>
2024-03-18 10:21:23 +0700

    Praktikum 3 : Fungsi

---
+++ Pertemuan_5_PHP02/fungsi.php
@@ -0,0 +1,10 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Yudha <br> ";
+    echo "Senang berkenalan dengan anda<br>";
+}
+
+perkenalan();
+perkenalan();
+?>
