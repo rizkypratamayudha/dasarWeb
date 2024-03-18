8a8bc3dc16efa657ff6606a60fca03eef4602cfd
rizkypratamayudha <masrizki87@gmail.com>
2024-03-18 10:47:33 +0700

    Praktikum 3 : Fungsi - fungsi return

--- Pertemuan_5_PHP02/fungsi.php
+++ Pertemuan_5_PHP02/fungsi.php
@@ -1,10 +1,31 @@
 <?php
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Yudha <br> ";
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam. ", ";
+    echo "Perkenalkan, nama saya ".$nama."<br>";
+
+    //memanggil fungsi lain
+    echo "Saya berusia ".hitungumur(2004,2024)." tahun <br>";
     echo "Senang berkenalan dengan anda<br>";
 }
 
-perkenalan();
-perkenalan();
+//memanggil fungsi yang sudah dibuat 
+perkenalan("Yudha");
+
+echo "<hr>";
+
+$saya ="Elok";
+$ucapansalam = "Selamat pagi";
+perkenalan($saya, $ucapansalam);
+
+echo "<hr>";
+perkenalan($saya);
+
+echo "<hr>";
+
+function hitungumur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+echo "Umur saya adalah ". hitungumur(2004, 2024) ." tahun";
+
 ?>
