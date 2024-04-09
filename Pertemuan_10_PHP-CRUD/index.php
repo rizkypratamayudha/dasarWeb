efdc49b04cc8bb8e754d6df11696f6ac790fa7f3
rizkypratamayudha <masrizki87@gmail.com>
2024-04-09 21:46:38 +0700

    Praktikum 1 : CRUD bagian read

---
+++ Pertemuan_10_PHP-CRUD/index.php
@@ -0,0 +1,13 @@
+<?php
+//koneksi database
+$koneksi = mysqli_connect("localhost","root","","prakwebdb");
+
+//memeriksa koneksi
+if(mysqli_connect_errno()){
+    die("Koneki database gagal : " . mysqli_connect_error());
+}
+else{
+    echo "Koneksi berhasil";
+}
+
+?>
