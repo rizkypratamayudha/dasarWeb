77314b0f59c8ec3279fa938fbf8396a5fdda84f3
rizkypratamayudha <masrizki87@gmail.com>
2024-04-11 12:27:49 +0700

    Prak 2,3,4 : create, edit, delete

--- Pertemuan_10_PHP-CRUD/crud/proses.php
+++ Pertemuan_10_PHP-CRUD/crud/proses.php
@@ -18,6 +18,44 @@ if($aksi == 'tambah'){
         echo "Gagal menambahkan data: " . mysqli_error($koneksi);
     }
 }
+else if ($aksi == 'ubah'){
+    if(isset($_POST['id'])){
+        $id = $_POST['id'];
+
+        $query = "UPDATE anggota SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id = $id";
+        if (mysqli_query($koneksi, $query)){
+            header("Location: index.php");
+            exit();
+        }
+        else {
+            echo "Gagal mengupdate data: " . mysqli_error($koneksi);
+        }
+    } 
+    else {
+        echo "id tidak valid";
+    }
+}
+else if ($aksi == 'hapus'){
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+
+        $query = "DELETE FROM anggota WHERE id = $id";
+        if(mysqli_query($koneksi,$query)){
+            header("Location: index.php");
+            exit();
+        }
+        else {
+            echo "Gagal menghapus data: " . mysqli_error($koneksi);
+        }
+    }
+    else {
+        echo "id tidak valid";
+    }
+}
+
+else {
+    header("Location: index.php");
+}
 
 mysqli_close($koneksi)
 ?>
