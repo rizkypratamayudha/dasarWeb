13662bb71c33bfa4479e0a3fc00f635adf4a89dc
rizkypratamayudha <masrizki87@gmail.com>
2024-04-15 14:07:53 +0700

    Praktikum 3 : Folder template

---
+++ Pertemuan_11_Web dinamis/admin/template/footer.php
@@ -0,0 +1,20 @@
+<!-- Praktikum 3. Folder Template Langkah 3 footer.php -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Bagian head biasanya berisi metadata dan referensi ke file eksternal, seperti CSS dan JavaScript -->
+    </head>
+    <body>
+        <!-- Bagian body adalah tempat konten utama halaman web ditampilkan kepada pengguna -->
+
+        <!-- Memuat skrip Bootstrap dan Chart.js untuk digunakan dalam halaman dashboard -->
+        <script src="assets/dist/js/bootstrap.bundle.min.js"></script> <!-- Memuat skrip Bootstrap -->
+        
+        <!-- Memuat skrip Chart.js dari CDN -->
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity=
+        "sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBXD+L6SEG4zoLp" crossorigin="anonymous"></script>
+        
+        <!-- Memuat skrip kustom untuk dashboard -->
+        <script src="assets/custom/dashboard.js"></script>
+    </body>
+</html>
